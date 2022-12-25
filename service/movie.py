55b1af0cda8761d6c5abb31717a86cbad7959d35
vital <vitaliykladko13@gmail.com>
2022-12-25 21:54:30 +0300

    commit_2

--- service/movie.py
+++ service/movie.py
@@ -34,6 +34,7 @@ class MovieService:
         if "director_id" in movie_d:
             movie.director_id = movie_d.get("director_id")
         self.dao.update(movie)
+        return movie
 
     def delete(self, rid):
         self.dao.delete(rid)
